bd89876978c01bf90e79d2de01e744d2f68aed2c
domieee <99103235+domieee@users.noreply.github.com>
2023-01-06 12:27:32 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function math(n1, n2) {
+        // Nimmt zwei Summen als Parameter entgegen
+
+    console.log(n1 * n2);
+        // Multipliziert beide Summen und gibt das Ergebniss in der Konsole aus
+
+    console.log(n1 / n2);
+        // Dividiert beide Summen und gibt das Ergebniss in der Konsole aus
+
+}
+
+math(10,2);
